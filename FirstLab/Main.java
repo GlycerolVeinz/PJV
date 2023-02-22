4a52518a96acc6846f5a13800f594c3bd35218de
GlycerolVeinz <safrankou.matvei@gmail.com>
2023-02-22 19:05:28 +0100

    trying out Java, for the first time

---
+++ FirstLab/Main.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner scanf = new Scanner(System.in);
+		String operation_s, operand1_s, operand2_s;
+
+		System.out.println("Vyberte operaci (1-soucet, 2-rozdil, 3-soucin, 4-podil):");
+		int operation = scanf.nextInt();
+		switch (operation) {
+			case 1:
+				operand1_s = "scitanec";
+				operand2_s = "scitanec";
+				operation_s = "s";
+				break;
+
+			case 2:
+				operand1_s = "mensenec";
+				operand2_s = "mensitel";
+				operation_s = "-";
+				break;
+
+			case 3:
+				operand1_s = "cinitel";
+				operand2_s = "cinitel";
+				operation_s = "*";
+				break;
+
+			case 4:
+				operand1_s = "delenec";
+				operand2_s = "delitel";
+				operation_s = "/";
+				break;
+
+			default:
+				System.err.println("Chybna volba");
+				break;
+		}
+
+		scanf.close();
+	}
+}

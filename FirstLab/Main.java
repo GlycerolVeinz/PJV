3e592d6fd79eda185b03660b80bb31fa78ffdc9e
GlycerolVeinz <safrankou.matvei@gmail.com>
2023-02-24 08:07:09 +0100

    added interaction with user

--- FirstLab/Main.java
+++ FirstLab/Main.java
@@ -3,7 +3,9 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
 		Scanner scanf = new Scanner(System.in);
-		String operation_s, operand1_s, operand2_s;
+		String operation_s = null;
+		String operand1_s = null;
+		String operand2_s = null;
 
 		System.out.println("Vyberte operaci (1-soucet, 2-rozdil, 3-soucin, 4-podil):");
 		int operation = scanf.nextInt();
@@ -11,7 +13,7 @@ public class Main {
 			case 1:
 				operand1_s = "scitanec";
 				operand2_s = "scitanec";
-				operation_s = "s";
+				operation_s = "+";
 				break;
 
 			case 2:
@@ -34,8 +36,15 @@ public class Main {
 
 			default:
 				System.err.println("Chybna volba");
+				scanf.close();
+				System.exit(100);
 				break;
 		}
+
+		System.out.printf("Zadej %s: ", operand1_s);
+		float operand1 = scanf.nextFloat();
+		System.out.printf("Zadej %s: ", operand2_s);
+		float opernad2 = scanf.nextFloat();
 
 		scanf.close();
 	}

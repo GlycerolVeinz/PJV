9dab131e83a0091ef60938db4066546b089490fa
GlycerolVeinz <safrankou.matvei@gmail.com>
2023-02-24 11:44:32 +0100

    finished HW1 version 1.0

--- FirstLab/Main.java
+++ FirstLab/Main.java
@@ -6,34 +6,34 @@ public class Main {
 		Scanner scanf = new Scanner(System.in);
 		DecimalFormat dform = new DecimalFormat();
 		String operation_s = null;
-		String operand1_s = null;
-		String operand2_s = null;
+		String operand1_name = null;
+		String operand2_name = null;
 
 		System.out.println("Vyberte operaci (1-soucet, 2-rozdil, 3-soucin, 4-podil):");
 		int operation = scanf.nextInt();
 		// prepare strings for last output
 		switch (operation) {
 			case 1:
-				operand1_s = "scitanec";
-				operand2_s = "scitanec";
+				operand1_name = "scitanec";
+				operand2_name = "scitanec";
 				operation_s = "+";
 				break;
 
 			case 2:
-				operand1_s = "mensenec";
-				operand2_s = "mensitel";
+				operand1_name = "mensenec";
+				operand2_name = "mensitel";
 				operation_s = "-";
 				break;
 
 			case 3:
-				operand1_s = "cinitel";
-				operand2_s = "cinitel";
+				operand1_name = "cinitel";
+				operand2_name = "cinitel";
 				operation_s = "*";
 				break;
 
 			case 4:
-				operand1_s = "delenec";
-				operand2_s = "delitel";
+				operand1_name = "delenec";
+				operand2_name = "delitel";
 				operation_s = "/";
 				break;
 
@@ -46,12 +46,19 @@ public class Main {
 		}
 
 		// ask user for numbers
-		System.out.printf("Zadej %s: \n", operand1_s);
+		System.out.printf("Zadej %s: \n", operand1_name);
 		float operand1 = scanf.nextFloat();
-		System.out.printf("Zadej %s: \n", operand2_s);
+		System.out.printf("Zadej %s: \n", operand2_name);
 		float operand2 = scanf.nextFloat();
 		System.out.println("Zadej pocet desetinych mist: ");
 		int cipher_places = scanf.nextInt();
+		if (cipher_places < 0) {
+			// can't have negative number of ciphers after decimal number
+			System.err.println("Chyba - Musi byt zadane kladne cislo!");
+			scanf.close();
+			System.exit(102);
+		}
+		dform.setMinimumFractionDigits(cipher_places);
 		dform.setMaximumFractionDigits(cipher_places);
 
 		// calculate result
@@ -68,12 +75,25 @@ public class Main {
 				break;
 			case 4:
 				if (operand2 == 0) {
+					// dividing with zero is undefined
+					System.err.println("Pokus o deleni nulou!");
+					scanf.close();
 					System.exit(101);
 				}
-				
+
 				result = operand1 / operand2;
 				break;
 		}
+
+		// format numbers
+		String form_result = dform.format(result);
+		String form_operand1 = dform.format(operand1);
+		String form_operand2 = dform.format(operand2);
+
+
+		// output result
+		System.out.printf("%s %s %s = %s\n", form_operand1, operation_s, form_operand2, form_result);
+
 		scanf.close();
 	}
 }

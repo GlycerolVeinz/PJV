43e554ca196aa571c676cb99fda25d2fe6608be4
GlycerolVeinz <safrankou.matvei@gmail.com>
2023-02-24 08:37:20 +0100

    fixed missspleling of operand2

--- FirstLab/Main.java
+++ FirstLab/Main.java
@@ -1,14 +1,17 @@
 import java.util.Scanner;
+import java.text.DecimalFormat;
 
 public class Main {
 	public static void main(String[] args) {
 		Scanner scanf = new Scanner(System.in);
+		DecimalFormat dform = new DecimalFormat();
 		String operation_s = null;
 		String operand1_s = null;
 		String operand2_s = null;
 
 		System.out.println("Vyberte operaci (1-soucet, 2-rozdil, 3-soucin, 4-podil):");
 		int operation = scanf.nextInt();
+		// prepare strings for last output
 		switch (operation) {
 			case 1:
 				operand1_s = "scitanec";
@@ -35,17 +38,42 @@ public class Main {
 				break;
 
 			default:
+				// exit if wrong thing is chosen
 				System.err.println("Chybna volba");
 				scanf.close();
 				System.exit(100);
 				break;
 		}
 
-		System.out.printf("Zadej %s: ", operand1_s);
+		// ask user for numbers
+		System.out.printf("Zadej %s: \n", operand1_s);
 		float operand1 = scanf.nextFloat();
-		System.out.printf("Zadej %s: ", operand2_s);
-		float opernad2 = scanf.nextFloat();
+		System.out.printf("Zadej %s: \n", operand2_s);
+		float operand2 = scanf.nextFloat();
+		System.out.println("Zadej pocet desetinych mist: ");
+		int cipher_places = scanf.nextInt();
+		dform.setMaximumFractionDigits(cipher_places);
 
+		// calculate result
+		float result = 0;
+		switch (operation) {
+			case 1:
+				result = operand1 + operand2;
+				break;
+			case 2:
+				result = operand1 - operand2;
+				break;
+			case 3:
+				result = operand1 * operand2;
+				break;
+			case 4:
+				if (operand2 == 0) {
+					System.exit(101);
+				}
+				
+				result = operand1 / operand2;
+				break;
+		}
 		scanf.close();
 	}
 }
